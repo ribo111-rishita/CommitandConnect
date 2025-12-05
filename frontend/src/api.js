345c74f2760b5d93c84075a736d48c42ef53c7a0
rishita.b006@gmail.com <rishitaboisnobi@Rishitas-MacBook-Pro.local>
2025-12-05 18:58:05 +0530

    added changes

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://commitandconnect.onrender.com/api",
   withCredentials: false,
 });
 
